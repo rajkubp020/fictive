f84cf49abc60a3d70139a7cc08353020e40ec482
testName <testemail>
2021-12-24 12:23:03 +0530

    try

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='fictive',
-   version='0.0.7',
+   version='0.0.8',
    description='Custom plots using matplotlib.',
    author='r Kumar',
    author_email='',
