f6ca8bc88374ddac1755019eb4ee0cf5765b2a43
testName <testemail>
2021-12-29 17:41:05 +0530

    R Kumar

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='fictive',
    version='3.2.2',
    description='Custom plots using matplotlib.',
-   author='r Kumar',
+   author='R Kumar',
    author_email='',
    packages=['fictive'],
    install_requires=['matplotlib', 'scipy', 'numpy']
